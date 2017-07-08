d054d9f6dafec70a422fd60e348ffc82f071649b
i_p_addr@yahoo.com <i_p_addr@yahoo.com>
2017-07-08 12:34:51 +0700

    add pep8 to code again

--- media.py
+++ media.py
@@ -4,7 +4,7 @@ import webbrowser
 # Class movie is reflect the blue print of movie
 # that we want to display in web browser
 class Movie():
-    # Init the class first time
+    # Init the class first time by passing title, storyline, poster and triler url
     def __init__(self, movieTitle, movieStoryline, moviePoster, movieTrialUrl):
         self.title = movieTitle
         self.storyline = movieStoryline
